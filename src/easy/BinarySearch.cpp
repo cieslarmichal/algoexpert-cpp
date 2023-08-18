0d70f253c8f1dadd8ac976896662b1883eafa883
Michal Cieslar <michalovskyyy@gmail.com>
2023-08-18 20:35:00 +0200

    add binary search problem solution

---
+++ src/easy/BinarySearch.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+int binarySearchHelper(std::vector<int> array, int target, int startIndex, int endIndex)
+{
+    const auto middleIndex = (startIndex + endIndex) / 2;
+
+    if (target == array[middleIndex])
+    {
+        return middleIndex;
+    }
+
+    if (startIndex == endIndex)
+    {
+        return -1;
+    }
+
+    if (target < array[middleIndex])
+    {
+        return binarySearchHelper(array, target, startIndex, middleIndex);
+    }
+    else
+    {
+        return binarySearchHelper(array, target, middleIndex + 1, endIndex);
+    }
+}
+
+int binarySearch(std::vector<int> array, int target)
+{
+    return binarySearchHelper(array, target, 0, array.size() - 1);
+}
